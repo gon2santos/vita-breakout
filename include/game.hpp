a1d96a75599eb5204ec3f2ca07f56b36c151b7dd
gon2santos <necchigonzalo@gmail.com>
2024-02-03 12:34:25 -0300

    add utilities

--- include/game.hpp
+++ include/game.hpp
@@ -1,5 +1,21 @@
+/*******************************************************************
+** This code is part of Breakout.
+**
+** Breakout is free software: you can redistribute it and/or modify
+** it under the terms of the CC BY 4.0 license as published by
+** Creative Commons, either version 4 of the License, or (at your
+** option) any later version.
+******************************************************************/
+
+#ifndef GAME_H
+#define GAME_H
+
+#include "glad.h"
+#include <SDL2/SDL.h>
+
 // Represents the current state of the game
-enum GameState {
+enum GameState
+{
     GAME_ACTIVE,
     GAME_MENU,
     GAME_WIN
@@ -22,3 +38,5 @@ public:
     void Update(float dt);
     void Render();
 };
+
+#endif
